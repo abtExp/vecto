b9221320b2ae84de4e0df8721345a08044eb02d1
Anubhav Tiwari <atworkstudios@gmail.com>
2017-05-27 20:34:14 +0530

    Nailed it...

---
+++ test.js
@@ -0,0 +1,8 @@
+const { Vector,sum,product } = require("./vectorize");
+
+let vect = new Vector([2,3,3]);
+vect.arrange();
+console.log(vect.array);
+
+console.log(sum(4,5));
+console.log(product(2,[1,2,3]));
