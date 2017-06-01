fcd372d2f01a0b09647904d54d1a31e82a74a3bd
Anubhav Tiwari <atworkstudios@gmail.com>
2017-06-01 18:44:16 +0530

    Added test coverage

--- test.js
+++ test.js
@@ -10,3 +10,13 @@ let v3 = new Vector([6]);
 v3.arrange();
 let sm = Vector.add(v2,v3);
 console.log(sm);
+
+let nv = Vector.array([[4,2,1],[7,8,9]]);
+console.log(nv.calc_shape(nv.array));
+console.log(nv.calc_size(nv.shape));
+console.log(nv);
+
+nv.reshape([6,1]);
+console.log(nv);
+nv.resize([5,5]);
+console.log(nv);
