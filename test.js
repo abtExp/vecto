c11919abe7e5a07093e0f2203850a5f88b2dc85c
Anubhav Tiwari <atworkstudios@gmail.com>
2017-06-07 12:26:19 +0530

    :construction:

--- test.js
+++ test.js
@@ -1,44 +1,44 @@
 const { Vector,sum,product } = require("./vecto");
 
-let v = Vector.zeroes([2]);
-console.log(v);
+// let v = Vector.zeroes([2]);
+// console.log(v);
 
-let v2 = Vector.zeroes([2,3]);
-console.log(v2);
-console.log(v2.size);
-let v3 = new Vector([6]);
-v3.arrange();
-let sm = Vector.add(v2,v3);
-console.log(sm);
+// let v2 = Vector.zeroes([2,3]);
+// console.log(v2);
+// console.log(v2.size);
+// let v3 = new Vector([6]);
+// v3.arrange();
+// let sm = Vector.add(v2,v3);
+// console.log(sm);
 
-let n2v = Vector.array([[4,2,1],[7,8,9]]);
-console.log(n2v.calc_shape(n2v.array));
-console.log(n2v.calc_size(n2v.shape));
-console.log(n2v);
+// let n2v = Vector.array([[4,2,1],[7,8,9]]);
+// console.log(Vector.calc_shape(n2v.array));
+// console.log(Vector.calc_size(n2v.shape));
+// console.log(n2v);
 
-let tnv = new Vector([2,2,3,4]);
-tnv.arrange(4);
-console.log(tnv);
-tnv.reshape([6,8]);
-console.log(tnv);
-n2v.reshape([6]);
-console.log(`reshaped = `,n2v);
-n2v.resize([5,5]);
-console.log(`resized = `,n2v);
+// let tnv = new Vector([2,2,3,4]);
+// tnv.arrange(4);
+// console.log(tnv);
+// tnv.reshape([6,8]);
+// console.log(tnv);
+// n2v.reshape([6]);
+// console.log(`reshaped = `,n2v);
+// n2v.resize([5,5]);
+// console.log(`resized = `,n2v);
 
-let nv = new Vector([2,3]);
-nv.arrange(2);
-console.log(nv.array);
-let nv2 = new Vector([2,3]);
-nv2.arrange(4);
-console.log(nv2.array);
-console.log(Vector.add(nv,nv2));
+// let nv = new Vector([2,3]);
+// nv.arrange(2);
+// console.log(nv.array);
+// let nv2 = new Vector([2,3]);
+// nv2.arrange(4);
+// console.log(nv2.array);
+// console.log(Vector.add(nv,nv2));
 
-let nnv = new Vector([5,5]);
-nnv.arrange(5);
-console.log(nnv);
+// let nnv = new Vector([5,5]);
+// nnv.arrange(5);
+// console.log(nnv);
 
-console.log(product(nnv.array,n2v.array));
+// console.log(product(nnv.array,n2v.array));
 
 let nvvn = new Vector([5,5]);
 nvvn.arrange(6);
---
+++ util/matrix_prod.js
@@ -0,0 +1,3 @@
+module.exports = function matrix_prod(m1,m2){
+    console.log("Bazziinnggaa");
+}
