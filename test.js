ab8db7ee89eb3730a3a4020222b93a8b0ff97bc2
Anubhav Tiwari <atworkstudios@gmail.com>
2017-06-09 19:46:21 +0530

    :hammer::wrench::sparkles:NowTheStepCountForFillMethodCanBeDefined

--- test.js
+++ test.js
@@ -1,82 +1,69 @@
 const { Vector,sum,product } = require("./vecto");
 
-// let v = Vector.zeroes([2]);
-// console.log(v);
+/* Tests for creation of Vector */
 
-// let v2 = Vector.zeroes([2,3]);
-// console.log(v2);
-// console.log(v2.size);
-// let v3 = new Vector([6]);
-// v3.arrange();
-// let sm = Vector.add(v2,v3); // Produces an uneven size error
-// console.log(sm);
+// : constructor(?shape[],?array[])
 
-// let n2v = Vector.array([[4,2,1],[7,8,9]]);
-// console.log(Vector.calc_shape(n2v.array));
-// console.log(Vector.calc_size(n2v.shape));
-// console.log(n2v);
+//case 1 : constructor(shape,arr)
+let v1 = new Vector([2,3],[[1,2,3],[4,5,6]]);
+console.log(v1);
 
-// let tnv = new Vector([2,2,3,4]);
-// tnv.arrange(4);
-// console.log(tnv);
-// tnv.reshape([6,8]);
-// console.log(tnv);
-// n2v.reshape([6]);
-// console.log(`reshaped = `,n2v);
-// n2v.resize([5,5]);
-// console.log(`resized = `,n2v);
+//case 2 : constructor(shape)
+let v2 = new Vector([2,3]);
+console.log(v2);
+// at this point the array and flat for v2 are empty arrays,
+// you've to call this.arrange() explicitly.
 
-// let nv = new Vector([2,3]);
-// nv.arrange(2);
-// console.log(nv.array);
-// let nv2 = new Vector([2,3]);
-// nv2.arrange(4);
-// console.log(nv2.array);
-// console.log(Vector.add(nv,nv2));
+//case 3 : constructor(array)
+let v3 = new Vector([],[[1,2,3],[4,5,6]]);
+console.log(v3);
 
-// let nnv = new Vector([5,5]);
-// nnv.arrange(5);
-// console.log(nnv);
-
-// console.log(product(nnv.array,n2v.array));
-
-// let nvvn = new Vector([5,5]);
-// nvvn.arrange(6);
-
-// let nv22 = new Vector(nvvn.shape);
-// nv22.arrange(5);
-
-// console.log(product(nvvn.array,nv22.array));
-// console.log(sum(nvvn.array,nv22.array));
-// console.log(sum(nvvn.array));
-// console.log(product(nvvn.array,[2,2,2,2,2]));
-// console.log(product(nvvn.array,2));
-
-// let c = [[2,3,4],[5,6,7]];
-// let d = [[2,4,5],[8,9,10]];
-
-// console.log(product(c,d));
+//case 4 : constructor()
+let v4 = new Vector();
+console.log(v4);
 
 
-// let vd = new Vector([2,4]);
-// vd.arrange(4);
+//: Vector.array(array)
 
-// let ta = new Vector([4]);
-// ta.arrange(5);
+let v5 = Vector.array([[1,2,3],[4,5,6]]);
+console.log(v5);
 
-// console.log(vd,ta);
+//: Vector.zeroes(shape)
 
-// console.log(product(vd.array,ta.array));
+let v6 = Vector.zeroes([2,3]);
+console.log(v6);
 
-// let b = new Vector([2,2,3]);
-// b.arrange();
-// console.log(b.array);
-// console.log(sum(b.array));
-// b.resize([6,2]);
-// console.log(b.array);
-// b.reshape([3,4]);
-// console.log(b.array);
 
+
+/* Tests for Vector.flatten(ndarr,target_arr) */
+
+
+
+/* Tests for Vector_object.arrange(?elems_arr,?fill_style) */
+
+
+
+/* Tests for product(arr1,arr2)*/
+
+
+
+/* Tests for sum(arr1,?arr2) */
+
+
+
+/* Tests for Vector.add(v2) */
+
+
+
+/* Tests for Vector_object.resize(new_shape[]) */
+
+
+
+/* Tests for Vector_object.reshape(new_shape[]) */
+
+
+
+/* Tests for Vector.fill(len,fill_style,...args) */
 let ta = Vector.fill(8);
 console.log(ta);
 ta = Vector.fill(8,"linear",1);
