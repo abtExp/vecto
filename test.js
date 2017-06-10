17b7c31c9b4d879bf9d294f042f251511223b5bb
Anubhav Tiwari <atworkstudios@gmail.com>
2017-06-10 21:01:45 +0530

    :bug::sparkles:

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { Vector,sum,product } = require("./vecto");
 
 /* Tests for creation of Vector */
 
-// : constructor(?shape[],?array[])
+//: constructor(?shape[],?array[])
 
 //case 1 : constructor(shape,arr)
 let v1 = new Vector([2,3],[[1,2,3],[4,5,6]]);
@@ -43,7 +43,7 @@ console.log(v6);
 
 /* Tests for Vector.flatten(ndarr,target_arr) */
 let v7 = [[[1,2,3],[4,5,6],[8,9,10]],[[2,3,4],[6,9,1],[4,5,2]],[[4,5,6],[1,4,1],[4,6,8]]];
-let tar1 = [];
+let tar1 = [];0
 Vector.flatten(v7,tar1);
 console.log(tar1);
 //Expected output : [1,2,3,4,5,6,8,9,10,2,3,4,6,9,1,4,5,2,4,5,6,1,4,1,4,6,8]
@@ -61,8 +61,59 @@ console.log(v8.array);
 v8.arrange([2]);
 console.log(v8.array);
 
-/* Tests for product(arr1,arr2)*/
+/* Tests for Vector_object.resize(new_shape[]) */
+let v9 = new Vector([2,2,3]);
+v9.arrange([4]);
+console.log(v9.array);
+v9.resize([2,9,5]);
+console.log(v9.array);
 
+/* Tests for Vector_object.reshape(new_shape[]) */
+let v10 = new Vector([2,3]);
+v10.arrange();
+console.log(v10.array);
+v10.reshape([6,2]);
+console.log(v10.array);
+
+
+/* Tests for Vector.fill(len,fill_style,...args) */
+
+let v11 = Vector.fill(8);
+console.log(v11);
+//Expected output : [random array of length 8];
+
+v11 = Vector.fill(8,"linear",1);
+console.log(v11);
+//Expected output : [1,1,1,1,1,1,1,1]
+
+v11 = Vector.fill(8,"linear",0.1,0.9,0.1);
+console.log(v11);
+//Expected output : [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8] 
+
+v11 = Vector.fill(8,"linear",10,20);
+console.log(v11);
+//Expected output : [10,11,12,13,14,15,16,17]
+
+
+/* Tests for product(arr1,arr2)*/
+let v12 = [[1,2,3],[3,4,1]],
+v13 = [10,10,10];
+console.log(product(v12,v13));
+
+let v14 = [[2,1,5],[6,7,8]];
+console.log(product(v12,v14));
+
+let v15 = [2,3,4];
+console.log(product(v13,v15));
+
+let v16 = [4,5,6,7,8];
+console.log(product(v12,v16));
+
+let v17 = [[1,2],[4,5]];
+console.log(product(v12,v17));
+
+console.log(product(v12,4));
+console.log(product(v12,v17));
 
 
 /* Tests for sum(arr1,?arr2) */
@@ -72,29 +123,3 @@ console.log(v8.array);
 /* Tests for Vector.add(v2) */
 
 
-
-/* Tests for Vector_object.resize(new_shape[]) */
-
-
-
-/* Tests for Vector_object.reshape(new_shape[]) */
-
-
-
-/* Tests for Vector.fill(len,fill_style,...args) */
-
-let ta = Vector.fill(8);
-console.log(ta);
-//Expected output : [random array of length 8];
-
-ta = Vector.fill(8,"linear",1);
-console.log(ta);
-//Expected output : [1,1,1,1,1,1,1,1]
-
-ta = Vector.fill(8,"linear",0.1,0.9,0.1);
-console.log(ta);
-//Expected output : [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8] 
-
-ta = Vector.fill(8,"linear",10,20);
-console.log(ta);
-//Expected output : [10,11,12,13,14,15,16,17]
