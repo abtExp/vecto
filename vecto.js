0966937265c162e8a6fe8ae0898f81cbdf50eb19
Anubhav Tiwari <atworkstudios@gmail.com>
2017-07-28 21:21:50 +0530

    updating branch

--- vecto.js
+++ vecto.js
@@ -2,7 +2,6 @@ const ndarray = require('./lib/ndarray');
 const sum = require('./util/sum');
 const product = require('./util/product');
 const core = require('./lib/core');
-
 module.exports = {
     ndarray : ndarray,
     sum : sum,
