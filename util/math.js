a73dbc0d9b41a0a27a769444780192405a1814f1
abtExp <atworkstudios@gmail.com>
2017-08-22 21:11:02 +0530

    added more util methods

--- util/math.js
+++ util/math.js
@@ -2,16 +2,16 @@
 
 const matmul = require('./matmul'),
     product = require('./product'),
-    sum = require('./sum');
-// divide = require('./divide'),   TODO
-// log = require('./log'),   TODO
-// exp = require('./exp');   TODO
+    sum = require('./sum'),
+    divide = require('./divide'),
+    log = require('./log'),
+    exp = require('./exp');
 
 module.exports = {
     matmul,
     product,
-    sum
-    // divide,
-    // log,
-    // exp
+    sum,
+    divide,
+    log,
+    exp
 }
