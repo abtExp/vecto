a6fad04433fafb10205058a6e98b290577b4e84c
abtExp <atworkstudios@gmail.com>
2017-09-02 18:20:09 +0530

    fixed sum for broadcasting and writing max&min

--- util/max.js
+++ util/max.js
@@ -17,29 +17,21 @@ function max(arg1, arg2, axis = 0) {
         }
         jump_len /= s1[axis];
         num_comp = axis < s1.length - 1 ? s1[s1.length - 1] : s1[s1.length - 2];
-        console.log(jump_len, inc, switches, num_comp);
 
         for (let i = 0; i < (ar1.length - jump_len); i++) {
-            console.log(`i = ${i}`);
             max = ar1[i];
             for (let j = 0; j < s1[axis] - 1; j++) {
-                console.log(`comparison no. = ${j}`);
-                console.log(`comparing arr[${i}] = ${ar1[i]} and arr[${i + ((j + 1) * jump_len)}] = ${ar1[i + ((j + 1) * jump_len)]}`);
                 if (ar1[i + ((j + 1) * jump_len)] > max) max = ar1[i + ((j + 1) * jump_len)];
-                console.log(max);
             }
             max_elems.push(max);
-            console.log(`the max array is now , ${max_elems}`);
             if ((i - 1) % num_comp === 0) {
-                i += (inc * axis * switches) - 1; //// Here's The error The Value of i will always be -1 if axis = 0
+                i += (inc * axis * switches) - 1;
                 switches++;
-                console.log(`i after jumping = ${i}`);
             }
         }
 
         return max_elems;
     }
-
     // else if(Array.isArray(arg1) && Array.isArray(arg2)){
 
     // }
