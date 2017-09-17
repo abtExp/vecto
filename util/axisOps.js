82b3a1c2d6ac5bbf608d8a82afb55b02c9a911b9
abtExp <atworkstudios@gmail.com>
2017-09-17 14:03:23 +0530

    fixing axisOps

--- util/axisOps.js
+++ util/axisOps.js
@@ -1,8 +1,9 @@
 module.exports = (shape, axis) => {
     const calc_size = require('../lib/calc_size');
-    let nComp = axis < shape.length ? shape[shape.length - 1] : shape[shape.length - 2],
+    let nComp = axis < shape.length - 1 ? shape[shape.length - 1] : 1,
         jumpLen = 1,
         inc = 1,
+        j = 0,
         size = calc_size(shape, 'shape'),
         switches = 1,
         elems = [];
@@ -15,15 +16,24 @@ module.exports = (shape, axis) => {
     console.log(`inc : ${inc}, jumpLen : ${jumpLen}`);
     console.log(size);
     for (let i = 0; i < (size - jumpLen); i++) {
-        console.log('looping');
         let inElems = [];
         inElems.push(i);
         for (let k = 1; k < shape[axis]; k++) {
             let fact = k * jumpLen + i;
+            console.log(`comparing : ${i} & ${fact}`);
             inElems.push(fact);
+            if (k >= size) {
+                return elems;
+            }
         }
         elems.push(inElems);
-        if (i % (nComp - 1) === 0 && i !== 0) i = (axis * inc * switches);
+        j++;
+        if (j % nComp === 0 && axis !== 0) {
+            j = 0;
+            i = (axis * inc * switches) - 1;
+            switches++;
+        }
+        console.log(`i = ${i}`);
     }
     return elems;
 }
