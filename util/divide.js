a483e445afad83ae3c06a806499d69d07c4ef67c
abtExp <atworkstudios@gmail.com>
2017-10-27 15:36:33 +0530

    divide

--- util/divide.js
+++ util/divide.js
@@ -17,6 +17,9 @@ function divide(arr1, arr2) {
     } else if (Array.isArray(arr1) && !Array.isArray(arr2)) {
         let t1 = form_arr(flatten(arr1), 'float32');
         return arrange(calc_shape(arr1), Array.from(t1.map(i => i / arr2)));
-    } else if (!Array.isArray(arr1) && Array.isArray(arr2)) return divide(arr2, arr1);
+    } else if (!Array.isArray(arr1) && Array.isArray(arr2)) {
+        let t2 = form_arr(flatten(arr2), 'float32');
+        return arrange(calc_shape(arr2), Array.from(t2.map(i => arr1 / i)));
+    }
 }
 module.exports = divide;
