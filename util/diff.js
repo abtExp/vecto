357a719fbcd6df45a9288c56ce83322d19e201c6
abtExp <atworkstudios@gmail.com>
2018-01-06 22:35:47 +0530

    fixing reference names

--- util/diff.js
+++ util/diff.js
@@ -1,7 +1,7 @@
 module.exports = function diff(arr1, arr2) {
-    const { flatten, form_arr, calc_shape, arrange } = require('../lib/core');
+    const { flatten, formArr, calcShape, arrange } = require('../lib/core');
     const { sum } = require('./math');
-    if (Array.isArray(arr2)) arr2 = arrange(calc_shape(arr2), form_arr(flatten(arr2)).map(i => -i));
+    if (Array.isArray(arr2)) arr2 = arrange(calcShape(arr2), formArr(flatten(arr2)).map(i => -i));
     else arr2 = -arr2;
     return sum(arr1, arr2);
 }
